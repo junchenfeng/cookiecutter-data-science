56d1e51ca2254f8a95bcb4a632db7b1cc2d4be70
rui.cheng <chengrui_tfc@163.com>
2020-06-15 15:05:42 +0800

    add post_gen_hook shebang line, add pre-commit file

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,4 +1,13 @@
-# A git hook file has to be excutable.
-PROJ_DIR=$PWD
-FILE="$PROJ_DIR/.git/hooks/pre-commit"
-chmod u+x $FILE
+#!/usr/bin/env bash
+
+# create a git pre commit hook
+# source file
+source_file="ci/pre-commit"
+echo $source_file
+
+# target file
+target_dir=".git/hooks"
+mkdir -p $target_dir
+
+mv $source_file $target_dir
+chmod u+x "$target_dir/pre-commit"
