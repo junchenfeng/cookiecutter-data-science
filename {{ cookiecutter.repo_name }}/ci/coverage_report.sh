107a455748657123555d091715a91f30eadacd11
rui.cheng <chengrui_tfc@163.com>
2020-06-16 11:46:23 +0800

    fix pipeline
    - fix bash scripts
    - fix execute commands

--- {{ cookiecutter.repo_name }}/ci/coverage_report.sh
+++ {{ cookiecutter.repo_name }}/ci/coverage_report.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
-source config.sh
-source dingding.sh
+source ci/config.sh
+source ci/dingding.sh
 
-RPOJ_DIR=$PWD
-COVERAGE_REPORT_FILE=$PROJ_DIR/coverage-reports/coverage.xml
+COVERAGE_REPORT_FILE="coverage-reports/coverage.xml"
 
-if [[ !-e $COVERAGE_REPORT_FILE ]];then
+if [[ ! -e $COVERAGE_REPORT_FILE ]];then
   echo "Coverage file does not exit, plz run test first."
   exit 1
 fi
 
 new_coverage_ratio=$(pipenv run diff-cover $COVERAGE_REPORT_FILE | grep -E 'Coverage:' | sed -e 's/[^0-9]//g')
-echo $new_coverage_ratio
+echo "$new_coverage_ratio"
 
-total_coverage_ratio=$(egrep 'line-rate="0\.[0-9]+"' $COVERAGE_REPORT_FILE | head -n 1 | sed -nE 's/^.+line-rate="0\.([0-9]{2}).+$/\1/p')
-echo $total_coverage_ratio
+total_coverage_ratio=$(egrep 'line-rate="(1|0\.[0-9]+)"' $COVERAGE_REPORT_FILE | head -1 | sed -nE 's/^.+line-rate="((1)|0\.([0-9]{2})).+$/\1/p')
+echo "$total_coverage_ratio"
 
-if [[ (-z "$new_coverage_ratio" || $new_coverage_ratio -gt $new_coverage_threshold)  && $total_coverage_ratio -ge $total_coverage_threshold ]]
+if [[ (-z "$new_coverage_ratio" || $new_coverage_ratio -gt $new_coverage_threshold)  && ($total_coverage_ratio -eq 1 || $total_coverage_ratio -ge $total_coverage_threshold) ]]
 then
   echo "测试覆盖率很好，请继续保持 ~"
   exit 0
@@ -32,7 +31,7 @@ else
   MESSAGE+="$MESSAGE  新增测试覆盖率为$new_coverage_ratio%。总体测试覆盖率为$total_coverage_ratio%。\n数值偏低, 请注意"
 fi
 
-echo $MESSAGE
-ding_notify $GITLAB_USER_EMAIL "测试覆盖率不合格" $MESSAGE
+echo "$MESSAGE"
+ding_notify "$GITLAB_USER_EMAIL" "测试覆盖率不合格" "$MESSAGE"
 
 exit 1
--- {{ cookiecutter.repo_name }}/ci/sonar_lint.sh
+++ {{ cookiecutter.repo_name }}/ci/sonar_lint.sh
@@ -1,17 +1,18 @@
-source config.sh
-source dingding.sh
+#!/usr/bin/env bash
+source ci/config.sh
+source ci/dingding.sh
 
 echo "[INFO] CI_PROJECT_NAME : $CI_PROJECT_NAME"
 echo "[INFO] CI_COMMIT_REF_NAME : $CI_COMMIT_REF_NAME"
 echo "[INFO] GITLAB_USER_EMAIL: $GITLAB_USER_EMAIL"
 
 
-sonar-scanner -X -Dsonar.projectKey=$CI_PROJECT_NAME \
+sonar-scanner -X -Dsonar.projectKey="$CI_PROJECT_NAME" \
                  -Dsonar.sources=. \
-                 -Dsonar.host.url=$sonar_host \
+                 -Dsonar.host.url="$sonar_host" \
                  -Dsonar.qualitygate.wait=true \
-                 -Dsonar.login=$sonar_login_token \
-                 -Dsonar.branch.name=$CI_COMMIT_REF_NAME
+                 -Dsonar.login="$sonar_login_token" \
+                 -Dsonar.branch.name="$CI_COMMIT_REF_NAME"
 
 # wait for code scanning by sonar server
 sleep 7
@@ -19,17 +20,12 @@ sleep 7
 API="$sonar_host/api/qualitygates/project_status?projectKey=$CI_PROJECT_NAME&branch=$CI_COMMIT_REF_NAME"
 echo "[\033[34;1mINFO\033[0m] Fetch SonarQube QualityGates Result From $API"
 
-RESULT=`curl -s "$API"`
-
-if [ "$DEBUG" != "" ];then
-    echo "[\033[34;1mINFO\033[0m] API Result : $RESULT"
-fi
-
-echo "[INFO] API: $API"
+RESULT=$(curl -s "$API")
 echo "[INFO] API Result : $RESULT"
-echo $RESULT | grep -E '\"ERROR\"'
 
-if [ -z "`echo $RESULT | grep -E '\"ERROR\"'`" ];then
+sonar_error=$(echo "$RESULT" | grep -E '\"ERROR\"')
+echo "$sonar_error"
+if [ -z "$sonar_error" ];then
     echo "[\033[34;1mINFO\033[0m] \033[32;1mSonarQube 代码检测通过\033[0m"
     exit 0
 fi
@@ -45,7 +41,7 @@ MESSAGE="$MESSAGE \n \n → [Pipeline Job]($CI_JOB_URL)"
 MESSAGE="$MESSAGE \n \n → [SonarQube Report](http://sonar.bg.huohua.cn/dashboard?branch=$CI_COMMIT_REF_NAME&id=$CI_PROJECT_NAME)"
 
 echo "[\033[34;1mINFO\033[0m] 发送钉钉通知"
-echo $MESSAGE
-ding_notify $GITLAB_USER_EMAIL "Sonar Lint 检测失败" $MESSAGE
+echo "$MESSAGE"
+ding_notify "$GITLAB_USER_EMAIL" "Sonar Lint 检测失败" "$MESSAGE"
 
 exit 1
